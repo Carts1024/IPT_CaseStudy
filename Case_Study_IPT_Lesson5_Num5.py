4369da93986f55fb8e73cdbd2d2a5a72173ab990
maeloraine <135142565+maeloraine@users.noreply.github.com>
2024-04-24 15:32:18 +0800

    Update Case_Study_IPT_Lesson5_Num5.py

--- Case_Study_IPT_Lesson5_Num5.py
+++ Case_Study_IPT_Lesson5_Num5.py
@@ -25,3 +25,33 @@ def SystemLogin():
         SystemLogin()
     
 SystemLogin()
+
+# --- SI RAINE TO HEHE GAWA KO DITO SA BABA
+
+# Dictionary with usernames and passwords
+systemUsers = {
+    "Mae": "Loraine",
+    "Ymata": "Dela Torre",
+    "Crystal": "Lyn",
+    "Rubio": "Danga",
+    "Lei": "Ann",
+    "Marribay": "Soreto",
+    "Carl": "Aldrey",
+    "Daulo": "Bergado",
+    "Clarisse": "Irish",
+    "Jotojot": "Paller"
+}
+
+# Input username and password
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+# Condition to check if the inputs match in the dictionary
+if username in systemUsers:  # Check if the username exist
+    if systemUsers[username] == password:  # When inputs match
+        print("\nYou have logged in successfully.")
+        print(f"Hello, {username}!")
+    else:  # When the user input a wrong password
+        print("\nInvalid password.")
+else:  # When the username does not exist
+    print(f"\nSorry, '{username}' is not a valid user.")
