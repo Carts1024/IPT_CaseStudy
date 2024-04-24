6fc56ee291c83d85330652df4397b8d0f4b76bf7
maeloraine <135142565+maeloraine@users.noreply.github.com>
2024-04-24 15:43:59 +0800

    Create Num1.py

---
+++ Case_Study_IPT_Lesson-4/Num1.py
@@ -0,0 +1,18 @@
+"""Define the variables x and y as lists of numbers 
+x=[1, 2, 3, 4, 5] 
+y=[11, 12, 13, 14, 15] 
+z=(21, 22, 23, 24, 25) 
+a. What is the value of 3*x? 
+b. What is the value of x+y? 
+c. What is the value of x-y? 
+d. What is the value of x[1]? 
+e. What is the value of x[0]?
+f. What is the value of x[-1]? 
+g. What is the value of x[:]? 
+h. What is the value of x[2:4]? 
+i. What is the value of x[1:4:2]? 
+j. What is the value of x[:2]? 
+k. What is the value of x[::2]? 
+l. What is the result of the following two expressions? 
+x[3]=8 
+print x"""
