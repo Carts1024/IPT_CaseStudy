9eaa12febda47d3b0a8c44d61634e758e595e762
maeloraine <135142565+maeloraine@users.noreply.github.com>
2024-04-24 15:45:00 +0800

    Create Num2.py

---
+++ Case_Study_IPT_Lesson-4/Num2.py
@@ -0,0 +1,4 @@
+"""Create a list with the following six items: 67, 62.9, “hi”, False, 8, 67, ‘apple’, 6.5. 
+Begin with the empty list shown below, and add 8 statements to add each item, one per item.
+The first four statements should use the append method to append the item to the list,
+and the last four statements should use concatenation"""
