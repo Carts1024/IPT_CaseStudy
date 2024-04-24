9e883ef32362bb96e756e765d329483866503d63
maeloraine <135142565+maeloraine@users.noreply.github.com>
2024-04-24 15:49:55 +0800

    Update Num2.py

--- Case_Study_IPT_Lesson-4/Num2.py
+++ Case_Study_IPT_Lesson-4/Num2.py
@@ -2,3 +2,17 @@
 Begin with the empty list shown below, and add 8 statements to add each item, one per item.
 The first four statements should use the append method to append the item to the list,
 and the last four statements should use concatenation"""
+
+# Create an empty list
+list = []
+
+# Add values to list using append
+list.append(67)
+list.append(62.9)
+list.append("hi")
+list.append(False)
+
+# Add values to list using concatenation
+list += [8, 67, 'apple', 6.5]
+
+print(list)
