0c66b3b35a17b09f945a6760d4bdd3ccb865e567
maeloraine <135142565+maeloraine@users.noreply.github.com>
2024-04-24 17:54:45 +0800

    Update and rename Case_StudyIPT_MID_Lesson5.py to MID_Lesson5.py

--- LESSON-5/MID_Lesson5.py
+++ LESSON-5/MID_Lesson5.py
@@ -1,7 +1,8 @@
 import os
 
+
 def start():  # Main (Function 1)
-    
+
     print('--------------------------------------------------------------')
     print('               WELCOME TO DICTIONARY METHODS BY GROUP 4')
     print('                              MEMBERS:\n')
@@ -10,10 +11,12 @@ def start():  # Main (Function 1)
     print('                        DELA TORRE, MAE LORAINE')
     print('                        PALLER, CLARISSE IRISH')
     print('                        SORETO, LEI ANN\n')
-    input('                       PRESS ENTER TO CONTINUE\n--------------------------------------------------------------')
+    input(
+        '                       PRESS ENTER TO CONTINUE\n--------------------------------------------------------------')
     os.system('cls')
 
-def menu(): # Function for menu 
+
+def menu():  # Function for menu
 
     while True:
         os.system('cls')
@@ -28,17 +31,17 @@ def menu(): # Function for menu
         print('[6] Exit')
 
         choice = (input('\nEnter your choice: '))
-        if choice != "": #Checks if input is empty 
+        if choice != "":  # Checks if input is empty
             choice = int(choice)
-            if choice >= 1 or choice <=6:
+            if choice >= 1 or choice <= 6:
                 if choice == 1:
-                    Item_1()   # Calls months Fction     
+                    Item_1()  # Calls months Fction
                 elif choice == 2:
-                    Item_2() # Calls Alphabetical Function
+                    Item_2()  # Calls Alphabetical Function
                 elif choice == 3:
-                    Item_3() # Calls days_31 Function
+                    Item_3()  # Calls days_31 Function
                 elif choice == 4:
-                    Item_4() #Calls Sorting Function
+                    Item_4()  # Calls Sorting Function
                 elif choice == 5:
                     Item_5()
                 elif choice == 6:
@@ -48,39 +51,47 @@ def menu(): # Function for menu
         else:
             print("Enter valid choice")
 
+
 def Item_1():
     os.system('cls')
-    print('----------------------------------------------------------------------------------------------------------------------------')
-    print('                     WRITE A PYTHON PROGRAM TO CREATE A LIST OF TUPLES HAVING FIRST ELEMENT AS THE\n       NUMBER AND SECOND ELEMENT AS THE SQUARE OF THE NUMBER. INPUT: LIST = [1, 2, 3] OUTPUT: [(1, 1), (2, 4), (3, 9)]')
-    print('----------------------------------------------------------------------------------------------------------------------------\n')
-    #Initialize tuple
+    print(
+        '----------------------------------------------------------------------------------------------------------------------------')
+    print(
+        '                     WRITE A PYTHON PROGRAM TO CREATE A LIST OF TUPLES HAVING FIRST ELEMENT AS THE\n       NUMBER AND SECOND ELEMENT AS THE SQUARE OF THE NUMBER. INPUT: LIST = [1, 2, 3] OUTPUT: [(1, 1), (2, 4), (3, 9)]')
+    print(
+        '----------------------------------------------------------------------------------------------------------------------------\n')
+    # Initialize tuple
     squared = ()
 
     # Inputs in list
-    list = [1,2,3]
+    list = [1, 2, 3]
 
     # List for squared numbers
     sq_list = []
 
-    # Loop for squaring numbers  
+    # Loop for squaring numbers
     for i in list:
         sq = i * i
-        squared = (i,sq)
-        # Adding squared numbers tuple to the list 
+        squared = (i, sq)
+        # Adding squared numbers tuple to the list
         sq_list.append(squared)
         del squared
 
     # Print Results
     print("Input:", list)
-    print("Squared Numbers: ",sq_list)
+    print("Squared Numbers: ", sq_list)
     input('')
-    return # Back to menu
+    return  # Back to menu
+
 
 def Item_2():
     os.system('cls')
-    print('----------------------------------------------------------------------------------------------------------------------------')
-    print('                      WRITE A PROGRAM THAT WILL ASK USER TO INPUT A LIST OF INTEGER TUPLES.\n                      ASK ALSO FOR ANOTHER INTEGER VALUE AND ASSIGN IT TO K.\n                      OUTPUT THE TUPLE THATARE DIVISIBLE BY K.')
-    print('----------------------------------------------------------------------------------------------------------------------------\n')
+    print(
+        '----------------------------------------------------------------------------------------------------------------------------')
+    print(
+        '                      WRITE A PROGRAM THAT WILL ASK USER TO INPUT A LIST OF INTEGER TUPLES.\n                      ASK ALSO FOR ANOTHER INTEGER VALUE AND ASSIGN IT TO K.\n                      OUTPUT THE TUPLE THAT ARE DIVISIBLE BY K.')
+    print(
+        '----------------------------------------------------------------------------------------------------------------------------\n')
 
     # Variable initialization
     userTup = ()  # initializing empty tupke
@@ -102,13 +113,6 @@ def Item_2():
 
     while (True):
         try:
-            k = int(input("Enter an integer as divisor: "))
-            break
-        except ValueError:  # if the wrong type was input
-            print("Invalid input! Only Enter integer numbers!\n")
-
-    while (True):
-        try:
             # loop statement for updating the tuple/list
             while i <= elementCount:
                 userInput = int(input("Enter elements [%i]: " % (i)))
@@ -118,10 +122,16 @@ def Item_2():
         except ValueError:  # if the wrong type was input
             print("Invalid input! Only Enter integer numbers!\n")
 
+    while (True):
+        try:
+            k = int(input("Enter an integer as divisor: "))
+            break
+        except ValueError:  # if the wrong type was input
+            print("Invalid input! Only Enter integer numbers!\n")
 
     # converting the list to tuple after updating
     userTup = tuple(listTup)
-    print("List of tuples: ",  userTup)  # printing
+    print("List of tuples: ", userTup)  # printing
 
     # loop for elements divisible by k
     for x in userTup:
@@ -131,22 +141,25 @@ def Item_2():
     # printing
     print("List of tuples divisible by {}: ".format(k), divTup)
     input('')
-    return # Back to menu
+    return  # Back to menu
 
 
 def Item_3():
     # Initialize Disctionary for months and days
-    days = {'January':31, 'February':28, 'March':31, 'April':30, 'May':31, 'June':30, 'July':31, 'August':31, 'September':30, 'October':31, 'November':30, 'December':31}
+    days = {'January': 31, 'February': 28, 'March': 31, 'April': 30, 'May': 31, 'June': 30, 'July': 31, 'August': 31,
+            'September': 30, 'October': 31, 'November': 30, 'December': 31}
     days_display = days.items()
     os.system('cls')
-    print('----------------------------------------------------------------------------------------------------------------------------')
+    print(
+        '----------------------------------------------------------------------------------------------------------------------------')
     print('                      HERE IS A DICTIONARY OF THE DAYS IN THE MONTHS OF THE YEAR: \n')
     for keys, value in days_display:
-        print('                      ',keys, ' : ', value)
-    print('----------------------------------------------------------------------------------------------------------------------------\n')
+        print('                      ', keys, ' : ', value)
+    print(
+        '----------------------------------------------------------------------------------------------------------------------------\n')
     input('')
-    
-    def menu_Item3(): # Function for menu 
+
+    def menu_Item3():  # Function for menu
         while True:
             try:
                 os.system('cls')
@@ -159,15 +172,15 @@ def Item_3():
                 print('[4] Print (key-value) pairs sorted by number of days')
                 print('[5] Exit')
                 choice = int(input('\nEnter your choice: '))
-                if 1 <= choice <= 5: # Corrected condition
+                if 1 <= choice <= 5:  # Corrected condition
                     if choice == 1:
-                        Months()   # Calls months Function     
+                        Months()  # Calls months Function
                     elif choice == 2:
-                        alphabetical() # Calls Alphabetical Function
+                        alphabetical()  # Calls Alphabetical Function
                     elif choice == 3:
-                        days_31() # Calls days_31 Function
+                        days_31()  # Calls days_31 Function
                     elif choice == 4:
-                        sorted_days() # Calls Sorting Function
+                        sorted_days()  # Calls Sorting Function
                     elif choice == 5:
                         break  # Exit
                 else:
@@ -175,13 +188,13 @@ def Item_3():
             except ValueError:  # if the wrong type was input
                 print("Invalid input! Only Enter integer numbers!\n")
 
-    def Months(): # Function for asking the user to enter a month name and use the dictionary to tell them how many days are in the month. 
+    def Months():  # Function for asking the user to enter a month name and use the dictionary to tell them how many days are in the month.
         os.system('cls')
-        while True: 
+        while True:
             Month = input("Enter a month name to know how many days are there in that month: ")
-            if len(Month) != 0: # Checks if the user entered a value
+            if len(Month) != 0:  # Checks if the user entered a value
                 Month = Month[0].upper() + Month[1:]
-                if Month in days: # Checks if the input is in the dictionary
+                if Month in days:  # Checks if the input is in the dictionary
                     break
                 else:
                     print("Enter a valid input")
@@ -189,41 +202,46 @@ def Item_3():
                 print("Enter a valid input")
         print(days.get(Month))
         input('')
-        return # Back to the menu
-        
-    def alphabetical(): # Function for printing out all of the keys in alphabetical order. 
-        
+        return  # Back to the menu
+
+    def alphabetical():  # Function for printing out all of the keys in alphabetical order.
+
         os.system('cls')
         sorted_keys = sorted(days.keys())
         for key in sorted_keys:
             print(key)
         input('')
-        return # Back to the menu
+        return  # Back to the menu
 
-    def days_31(): # Function for printing out all of the months with 31 days. 
+    def days_31():  # Function for printing out all of the months with 31 days.
         os.system('cls')
         print('MONTHS WITH 31 DAYS: \n')
         for key in days:
             if days[key] == 31:
                 print(key)
         input('')
-        return # Back to the menu
+        return  # Back to the menu
 
-    def sorted_days(): # Function for printing out the (key-value) pairs sorted by the number of days in each month
-        os.system('cls') 
+    def sorted_days():  # Function for printing out the (key-value) pairs sorted by the number of days in each month
+        os.system('cls')
         sorted_days = sorted(days.items(), key=lambda x: x[1], reverse=True)
         for key, value in sorted_days:
             print(key, ':', value)
         input('')
         return  # Back to the menu
+
     menu_Item3()
     return
 
+
 def Item_4():
     os.system('cls')
-    print('----------------------------------------------------------------------------------------------------------------------------')
-    print('           WRITE A PROGRAM THAT REPEATEDLY ASKS THE USER TO ENTER PRODUCT NAMES AND PRICES.\n           STORE ALL OF THESE IN A DICTIONARY WHOSE KEYS ARE THE PRODUCT NAMES AND WHOSE VALUES ARE THE PRICES.\n           WHEN THE USER IS DONE ENTERING PRODUCTS AND PRICES, ALLOW THEM TO REPEATEDLY ENTER A PRODUCT NAME AND\n           PRINT THE CORRESPONDING PRICE OR A MESSAGE IF THE PRODUCT IS NOT IN THE DICTIONARY.')
-    print('----------------------------------------------------------------------------------------------------------------------------\n')
+    print(
+        '----------------------------------------------------------------------------------------------------------------------------')
+    print(
+        '           WRITE A PROGRAM THAT REPEATEDLY ASKS THE USER TO ENTER PRODUCT NAMES AND PRICES.\n           STORE ALL OF THESE IN A DICTIONARY WHOSE KEYS ARE THE PRODUCT NAMES AND WHOSE VALUES ARE THE PRICES.\n           WHEN THE USER IS DONE ENTERING PRODUCTS AND PRICES, ALLOW THEM TO REPEATEDLY ENTER A PRODUCT NAME AND\n           PRINT THE CORRESPONDING PRICE OR A MESSAGE IF THE PRODUCT IS NOT IN THE DICTIONARY.')
+    print(
+        '----------------------------------------------------------------------------------------------------------------------------\n')
     input('')
     # Sample dictionary
     products = {}
@@ -234,10 +252,10 @@ def Item_4():
         input_price = float(input('Enter a price: '))
         products[input_product] = input_price
 
-    def repeat():   
+    def repeat():
         choice = (str(input('Do you want to enter again? (Press Y to continue): '))).upper()
         return choice
-    
+
     def ask():
         choice = 'Y'
         while (choice == 'Y'):
@@ -249,7 +267,7 @@ def Item_4():
             else:
                 print('The item is not in the dictionary')
             choice = repeat()
-            
+
     os.system('cls')
     choice = 'Y'
     while (choice == 'Y'):
@@ -260,10 +278,11 @@ def Item_4():
     input('')
     return
 
+
 def Item_5():
     # Dictionary with usernames and passwords
     systemUsers = {
-        'Mae': "13",
+        'Mae': "Loraine",
         "Ymata": "Dela Torre",
         "Crystal": "Lyn",
         "Rubio": "Danga",
@@ -275,13 +294,16 @@ def Item_5():
         "Jotojot": "Paller"
     }
     os.system('cls')
-    print('--------------------------------------------------------------------------------------------------------------------------------------------------')
-    print('     WRITE A PROGRAM THAT USES A DICTIONARY THAT CONTAINS TEN USER NAMES AND PASSWORDS.\n     THE PROGRAM SHOULD ASK THE USER TO ENTER THEIR USERNAME AND PASSWORD.\n     IF THE USERNAME IS NOT IN THE DICTIONARY, THE PROGRAM SHOULD INDICATE THAT THE PERSON IS NOT A VALID USER OF THE SYSTEM.\n     IF THE USERNAME IS IN THE DICTIONARY, BUT THE USER DOES NOT ENTER THE RIGHT PASSWORD, THE PROGRAM SHOULD SAY THAT THE PASSWORD IS INVALID.\n     IF THE PASSWORD IS CORRECT, THEN THE PROGRAM SHOULD TELL THE USER THAT THEY ARE NOW LOGGED IN TO THE SYSTEM.')
-    print('--------------------------------------------------------------------------------------------------------------------------------------------------\n')
+    print(
+        '--------------------------------------------------------------------------------------------------------------------------------------------------')
+    print(
+        '     WRITE A PROGRAM THAT USES A DICTIONARY THAT CONTAINS TEN USER NAMES AND PASSWORDS.\n     THE PROGRAM SHOULD ASK THE USER TO ENTER THEIR USERNAME AND PASSWORD.\n     IF THE USERNAME IS NOT IN THE DICTIONARY, THE PROGRAM SHOULD INDICATE THAT THE PERSON IS NOT A VALID USER OF THE SYSTEM.\n     IF THE USERNAME IS IN THE DICTIONARY, BUT THE USER DOES NOT ENTER THE RIGHT PASSWORD, THE PROGRAM SHOULD SAY THAT THE PASSWORD IS INVALID.\n     IF THE PASSWORD IS CORRECT, THEN THE PROGRAM SHOULD TELL THE USER THAT THEY ARE NOW LOGGED IN TO THE SYSTEM.')
+    print(
+        '--------------------------------------------------------------------------------------------------------------------------------------------------\n')
     input('')
     choice = 'Y'
     while (choice == 'Y'):
-        
+
         os.system('cls')
         users = systemUsers.items()
         for key, value in users:
@@ -309,11 +331,9 @@ def Item_5():
             print(f"\nSorry, '{username}' is not a valid user.")
             continue
     return
-    
-        
+
 
 if __name__ == "__main__":
     os.system('cls')
     start()
     menu()
-
